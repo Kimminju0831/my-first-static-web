8d657c73785d26b71e30dea13f27d0eefc4c6155
ruth <kimminju77786@gmail.com>
2023-07-15 11:46:32 +0900

    hi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./components/Game";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById("root"));
